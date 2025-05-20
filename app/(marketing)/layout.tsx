27096af4664731bc93b085fb85316f391edf149b
Aditya kumar <ak6101283@gmail.com>
2025-05-20 15:23:49 +0530

    update

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  children: React.ReactNode;
+};
+import {Header }from "./header";
+const layout = ({ children }: Props) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+        <Header/>
+      <main className=" flex-1 flex flex-col items-center justify-center">{children}</main>
+    </div>
+  );
+};
+
+export default layout;
