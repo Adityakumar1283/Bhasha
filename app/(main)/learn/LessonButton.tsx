1cd36e7cad0baaa861bd24ecba51d76a41694522
Aditya kumar <ak6101283@gmail.com>
2025-06-08 15:23:38 +0530

    making lesson button ui

--- app/(main)/learn/LessonButton.tsx
+++ app/(main)/learn/LessonButton.tsx
@@ -1,24 +1,103 @@
-"use client"
+"use client";
+import Link from "next/link";
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import { cn } from "@/lib/utils";
+import "react-circular-progressbar";
+import { Check, Crown, Star } from "lucide-react";
+import { Button } from "@/components/ui/button";
+type Props = {
+  id: number;
+  index: number;
+  totalCount: number;
+  locked?: boolean;
+  current?: boolean;
+  percentage: number;
+};
 
-type Props ={
-    id:number;
-    index:number;
-    totalCount:number;
-    locked?:boolean;
-    current?:boolean;
-    percentage:number;
+const LessonButton = ({
+  id,
+  index,
+  totalCount,
+  locked,
+  current,
+  percentage,
+}: Props) => {
+  const cycleLength = 8;
+  const cycleIndex = index % cycleLength;
 
-}
+  let indentationLevel;
 
+  if (cycleIndex <= 2) {
+    indentationLevel = cycleIndex;
+  } else if (cycleIndex <= 4) {
+    indentationLevel = 4 - cycleIndex;
+  } else if (cycleIndex <= 6) {
+    indentationLevel = 4 - cycleIndex;
+  } else {
+    indentationLevel = cycleIndex - 8;
+  }
+  const rigthPosition = indentationLevel * 40;
+  const isFirst = index === 0;
+  const isLast = index === totalCount;
+  const isCompeleted = !current && !locked;
+  const Icon = isCompeleted ? Check : isLast ? Crown : Star;
 
+  const href = isCompeleted ? `/lesson${id}` : "/lesson";
 
-const LessonButton = ({id,index,totalCount,locked,current,percentage}:Props) => {
   return (
-    <div>
-      {id}
-      
-    </div>
-  )
-}
+    <Link
+      href={href}
+      aria-disabled={locked}
+      style={{ pointerEvents: locked ? "none" : "auto" }}
+    >
+      <div
+        className="relative"
+        style={{
+          right: `${rigthPosition}px`,
+          marginTop: isFirst && !isCompeleted ? 60 : 24,
+        }}
+      >
+        {current ? (
+          <div className="h-[102px] w-[120]px relative">
+            <div className="absolute -top-6 left-2.5 px-3 py-2.5 border-2 font-bold uppercase text-green-500 bg-white rounded-xl animate-bounce tracking-widez-10 z-20">
+              start
+              <div className="absolute left-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-x-1/2" />
+            </div>
+            <CircularProgressbarWithChildren
+              value={Number.isNaN(percentage) ? 0 : percentage}
+              styles={{
+                path: {
+                  stroke: "#4ade80",
+                },
+                trail: {
+                  stroke: "e5e7eb",
+                },
+              }}
+            >
+              <Button
+                size="rounded"
+                variant={locked ? "locked" : "secondary"}
+                className="h-[70px] w-[70px] border-b-8"
+              >
+                <Icon
+                  className={cn(
+                    "h-10 w-10 ",
+                    locked
+                      ? " fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                      : "fill-primary text-primary-foreground ",
+                    isCompeleted && "fill-none stroke-[4]"
+                  )}
+                />
+              </Button>
+            </CircularProgressbarWithChildren>
+            {id}
+          </div>
+        ) : (
+          "something"
+        )}
+      </div>
+    </Link>
+  );
+};
 
-export default LessonButton
+export default LessonButton;
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,26 +9,35 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
+        locked:
+          "bg-neutral-200 text-primary-foreground hover:bg-neutral-200/90 border-neutral-400 border-b-4 active:border-b-0 ",
         default:
           "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate0-500",
-        primary:"bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
-        primaryOutline:" bg-white text-sky-500 hover:bg-slate-100",
-        secondary:"bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0 ",
-        secondaryOutline:" bg-white text-green-500 hover:bg-slate-100",
-        danger:"bg-rose-700 text-primary-foreground hover:bg-rose-600/90 border-rose-800 border-b-4 active:border-b-0",
-        dangerOutline:"bg-white text-rose-500 hover:bg-slate-100",
-        super:"bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0",
-        superOutline:"bg-white text-indigo-500 hover:bg-slate-100",
-        ghost:" bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
-        sidebar:" bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100 transition-none",
-        sidebarOutline:"bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none  "
+        primary:
+          "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
+        primaryOutline: " bg-white text-sky-500 hover:bg-slate-100",
+        secondary:
+          "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0 ",
+        secondaryOutline: " bg-white text-green-500 hover:bg-slate-100",
+        danger:
+          "bg-rose-700 text-primary-foreground hover:bg-rose-600/90 border-rose-800 border-b-4 active:border-b-0",
+        dangerOutline: "bg-white text-rose-500 hover:bg-slate-100",
+        super:
+          "bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0",
+        superOutline: "bg-white text-indigo-500 hover:bg-slate-100",
+        ghost:
+          " bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
+        sidebar:
+          " bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100 transition-none",
+        sidebarOutline:
+          "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none  ",
       },
       size: {
         default: "h-11 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8  gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-12  px-6 px-8",
         icon: "size-10",
-        rounded: " rounded-full "
+        rounded: " rounded-full ",
       },
     },
     defaultVariants: {
